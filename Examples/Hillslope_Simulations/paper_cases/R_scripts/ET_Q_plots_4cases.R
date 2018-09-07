b28c4eaa3aa9d9918d0c412dceb61a969048be0a
Reed Maxwell <rmaxwell@mines.edu>
2018-09-07 09:42:01 -0600

    added R scripts to make paper plots

---
+++ Examples/Hillslope_Simulations/paper_cases/R_scripts/ET_Q_plots_4cases.R
@@ -0,0 +1,203 @@
+### plot four panels with ET and Q for all four cases
+### RMM 25-May-18, (edited 7-Sep-18)
+
+##This script loads in EcoSLIM output and makes Figures 2 and 3 from Maxwell et al Ecohydrology 2018
+
+## you may need to edit the working directory 
+setwd('~/EcoSLIM/Examples/Hillslope_Simulations/paper_cases/R_scripts')
+
+## load shrubs with ER forcing
+filename='../ER_hillslope_shrub/SLIM_hillslope_ER_shrub_ET_output.txt'
+ET_er_shrub = matrix(scan(file=filename,skip=1),nrow=8760*20,ncol=7,byrow=T)
+
+filename='../ER_hillslope_shrub/SLIM_hillslope_ER_shrub_flow_output.txt'
+Outflow_er_shrub = matrix(scan(file=filename,skip=1),nrow=8760*20,ncol=7,byrow=T)
+
+filename='../ER_hillslope_shrub/SLIM_hillslope_ER_shrub_PET_balance.txt'
+PET_er_shrub = matrix(scan(file=filename,skip=1),nrow=8760*20,ncol=3,byrow=T)
+
+
+ET_day_er_shrub = matrix(0,nrow=20*365,ncol=7,byrow=T)
+Outflow_day_er_shrub =  matrix(0,nrow=20*365,ncol=7,byrow=T)
+PET_day_er_shrub =  matrix(0,nrow=20*365,ncol=3,byrow=T)
+
+for (i in 1:(20*365)) {
+	for (j in 1:7) {
+	ET_day_er_shrub[i,j] = mean(ET_er_shrub[((i-1)*24+1):(i*24),j])
+	Outflow_day_er_shrub[i,j] = mean(Outflow_er_shrub[((i-1)*24+1):(i*24),j])
+	}
+}
+
+for (i in 1:(20*365)) {
+	for (j in 1:3) {
+	PET_day_er_shrub[i,j] = mean(PET_er_shrub[((i-1)*24+1):(i*24),j])
+	}
+}
+options(scipen=5)
+
+## load trees with ER forcing
+filename='../ER_hillslope_trees/SLIM_hillslope_ER_trees_ET_output.txt'
+ET_er_trees = matrix(scan(file=filename,skip=1),nrow=8760*20,ncol=7,byrow=T)
+
+filename='../ER_hillslope_trees/SLIM_hillslope_ER_trees_flow_output.txt'
+Outflow_er_trees = matrix(scan(file=filename,skip=1),nrow=8760*20,ncol=7,byrow=T)
+
+filename='../ER_hillslope_trees/SLIM_hillslope_ER_trees_PET_balance.txt'
+PET_er_trees = matrix(scan(file=filename,skip=1),nrow=8760*20,ncol=3,byrow=T)
+
+
+ET_day_er_trees = matrix(0,nrow=20*365,ncol=7,byrow=T)
+Outflow_day_er_trees =  matrix(0,nrow=20*365,ncol=7,byrow=T)
+PET_day_er_trees =  matrix(0,nrow=20*365,ncol=3,byrow=T)
+
+for (i in 1:(20*365)) {
+	for (j in 1:7) {
+	ET_day_er_trees[i,j] = mean(ET_er_trees[((i-1)*24+1):(i*24),j])
+	Outflow_day_er_trees[i,j] = mean(Outflow_er_trees[((i-1)*24+1):(i*24),j])
+	}
+}
+
+for (i in 1:(20*365)) {
+	for (j in 1:3) {
+	PET_day_er_trees[i,j] = mean(PET_er_trees[((i-1)*24+1):(i*24),j])
+	}
+}
+options(scipen=5)
+
+## load shrubs with LW forcing
+filename='../LW_hillslope_shrub/SLIM_hillslope_LW_shrub_ET_output.txt'
+ET_lw_shrub = matrix(scan(file=filename,skip=1),nrow=8760*20,ncol=7,byrow=T)
+
+filename='../LW_hillslope_shrub/SLIM_hillslope_LW_shrub_flow_output.txt'
+Outflow_lw_shrub = matrix(scan(file=filename,skip=1),nrow=8760*20,ncol=7,byrow=T)
+
+filename='../LW_hillslope_shrub/SLIM_hillslope_LW_shrub_PET_balance.txt'
+PET_lw_shrub = matrix(scan(file=filename,skip=1),nrow=8760*20,ncol=3,byrow=T)
+
+
+ET_day_lw_shrub = matrix(0,nrow=20*365,ncol=7,byrow=T)
+Outflow_day_lw_shrub =  matrix(0,nrow=20*365,ncol=7,byrow=T)
+PET_day_lw_shrub =  matrix(0,nrow=20*365,ncol=3,byrow=T)
+
+for (i in 1:(20*365)) {
+	for (j in 1:7) {
+	ET_day_lw_shrub[i,j] = mean(ET_lw_shrub[((i-1)*24+1):(i*24),j])
+	Outflow_day_lw_shrub[i,j] = mean(Outflow_lw_shrub[((i-1)*24+1):(i*24),j])
+	}
+}
+
+for (i in 1:(20*365)) {
+	for (j in 1:3) {
+	PET_day_lw_shrub[i,j] = mean(PET_lw_shrub[((i-1)*24+1):(i*24),j])
+	}
+}
+
+## load trees with LW forcing
+filename='../LW_hillslope_trees/SLIM_hillslope_LW_trees_ET_output.txt'
+ET_lw_trees = matrix(scan(file=filename,skip=1),nrow=8760*20,ncol=7,byrow=T)
+
+filename='../LW_hillslope_trees/SLIM_hillslope_LW_trees_flow_output.txt'
+Outflow_lw_trees = matrix(scan(file=filename,skip=1),nrow=8760*20,ncol=7,byrow=T)
+
+filename='../LW_hillslope_trees/SLIM_hillslope_LW_trees_PET_balance.txt'
+PET_lw_trees = matrix(scan(file=filename,skip=1),nrow=8760*20,ncol=3,byrow=T)
+
+
+ET_day_lw_trees = matrix(0,nrow=20*365,ncol=7,byrow=T)
+Outflow_day_lw_trees =  matrix(0,nrow=20*365,ncol=7,byrow=T)
+PET_day_lw_trees =  matrix(0,nrow=20*365,ncol=3,byrow=T)
+
+for (i in 1:(20*365)) {
+	for (j in 1:7) {
+	ET_day_lw_trees[i,j] = mean(ET_lw_trees[((i-1)*24+1):(i*24),j])
+	Outflow_day_lw_trees[i,j] = mean(Outflow_lw_trees[((i-1)*24+1):(i*24),j])
+	}
+}
+
+for (i in 1:(20*365)) {
+	for (j in 1:3) {
+	PET_day_lw_trees[i,j] = mean(PET_lw_trees[((i-1)*24+1):(i*24),j])
+	}
+}
+
+
+
+
+## normalize ET and Q to mm/day
+# as total amount, convert to mm/d
+norm = (1000*200*1)/(24*1000)
+
+#plot flow and ET residence time comp Y20
+#
+fout = 'Figure2_Q-y20-4-panel.pdf'
+pdf(file=fout)
+par(mfrow=c(2,2))
+plot(Outflow_day_er_shrub[,1]/24/365,(Outflow_day_er_shrub[,3]+Outflow_day_er_shrub[,4]+Outflow_day_er_shrub[,5])*Outflow_day_er_shrub[,2]/24/365,cex=0.5,col="black",type='l',xlab='', ylab='Outflow [mm/d] and Residence Time [y]',xlim=c(19,20.),ylim=c(0,5.5),main="A) ER Shrubs",xaxt='n')
+axis(1, at=c(19+1/12,19+3/12,19+5/12,19+7/12,19+9/12,19+11/12), labels=c("Oct","Dec","Feb","Apr","Jun","Aug"))
+
+par(new=T)
+plot(Outflow_day_er_shrub[,1]/24/365,Outflow_day_er_shrub[,6]/norm,cex=0.5,col='blue',type='p',axes=F,xlim=c(19,20.),ylim=c(0,5.5),xlab="",ylab="") 
+legend('topright',c('Outflow Residence Time'),col=c('black'),lty=1,box.lwd = 0,cex=0.7)
+legend('topleft',c('Outflow'),col=c('blue'),pch=21,box.lwd = 0, cex=0.7)
+
+plot(Outflow_day_er_trees[,1]/24/365,(Outflow_day_er_trees[,3]+Outflow_day_er_trees[,4]+Outflow_day_er_trees[,5])*Outflow_day_er_trees[,2]/24/365,cex=0.5,col="black",type='l',xlab='', ylab='Outflow [mm/d] and Residence Time [y]',xlim=c(19,20.),ylim=c(0,5.5),main="B) ER Trees",xaxt='n')
+axis(1, at=c(19+1/12,19+3/12,19+5/12,19+7/12,19+9/12,19+11/12), labels=c("Oct","Dec","Feb","Apr","Jun","Aug"))
+
+par(new=T)
+plot(Outflow_day_er_trees[,1]/24/365,Outflow_day_er_trees[,6]/norm,cex=0.5,col='blue',type='p',axes=F,xlim=c(19,20.),ylim=c(0,5.5),xlab="",ylab="") 
+
+plot(Outflow_day_lw_shrub[,1]/24/365,(Outflow_day_lw_shrub[,3]+Outflow_day_lw_shrub[,4]+Outflow_day_lw_shrub[,5])*Outflow_day_lw_shrub[,2]/24/365,cex=0.5,col="black",type='l',xlab='', ylab='Outflow [mm/d] and Residence Time [y]',xlim=c(19,20.),ylim=c(0,5.5),main="C) LW Shrubs",xaxt='n')
+axis(1, at=c(19+1/12,19+3/12,19+5/12,19+7/12,19+9/12,19+11/12), labels=c("Oct","Dec","Feb","Apr","Jun","Aug"))
+
+par(new=T)
+plot(Outflow_day_lw_shrub[,1]/24/365,Outflow_day_lw_shrub[,6]/norm,cex=0.5,col='blue',type='p',axes=F,xlim=c(19,20.),ylim=c(0,5.5),xlab="",ylab="") 
+
+plot(Outflow_day_lw_trees[,1]/24/365,(Outflow_day_lw_trees[,3]+Outflow_day_lw_trees[,4]+Outflow_day_lw_trees[,5])*Outflow_day_lw_trees[,2]/24/365,cex=0.5,col="black",type='l',xlab="",xaxt='n', ylab='',xlim=c(19,20.),ylim=c(0,5.5),main="D) LW Trees")
+axis(1, at=c(19+1/12,19+3/12,19+5/12,19+7/12,19+9/12,19+11/12), labels=c("Oct","Dec","Feb","Apr","Jun","Aug"))
+
+par(new=T)
+plot(Outflow_day_lw_trees[,1]/24/365,Outflow_day_lw_trees[,6]/norm,cex=0.5,col='blue',type='p',axes=F,xlim=c(19,20.),ylim=c(0,5.5),xlab="",ylab="Outflow [mm/d] and Residence Time [y]") 
+
+dev.off()
+
+## normalize ET and Q to mm/day
+# as total amount, convert to mm/d
+norm = (1000*200*1)/(24*1000)
+
+#plot  ET residence time comp Y20
+fout = 'Figure3_ET-y20-4-panel.pdf'
+pdf(file=fout)
+
+par(mfrow=c(2,2))
+plot(ET_day_er_shrub[,1]/24/365,(ET_day_er_shrub[,3]+ET_day_er_shrub[,4]+ET_day_er_shrub[,5])*ET_day_er_shrub[,2]/24/365,cex=0.5,col="black",type='l',xlab='', ylab='ET [mm/d], ET Residence Time [y]',xlim=c(19,20.),ylim=c(0,1.5),main="A) ER Shrubs",xaxt='n')
+#points(Outflow_day_er_shrub[,1]/24/365,(Outflow_day_er_shrub[,3]+Outflow_day_er_shrub[,4])*Outflow_day_er_shrub[,2]/24/365,cex=0.5,col="blue",type='l')
+par(new=T)
+plot(ET_day_er_shrub[,1]/24/365,ET_day_er_shrub[,6]/norm,cex=0.5,col="blue",type='p',axes=F,xlim=c(19,20.),ylim=c(0,1.5),xlab="",ylab="") 
+axis(1, at=c(19+1/12,19+3/12,19+5/12,19+7/12,19+9/12,19+11/12), labels=c("Oct","Dec","Feb","Apr","Jun","Aug"))
+#axis(4, ylim=c(0,15/norm))
+#mtext(4,text="ET [mm/d]",level=2)
+legend('topright',c('ET Residence Time'),col=c('black'),lty=1,box.lwd = 0)
+legend('topleft',c('ET'),col=c('blue'),pch=21,box.lwd = 0)
+
+plot(ET_day_er_trees[,1]/24/365,(ET_day_er_trees[,3]+ET_day_er_trees[,4]+ET_day_er_trees[,5])*ET_day_er_trees[,2]/24/365,cex=0.5,col="black",type='l',xlab='', ylab='ET [mm/d], ET Residence Time [y]',xlim=c(19,20.),ylim=c(0,1.5),main="B) ER Trees",xaxt='n')
+#points(Outflow_day_er_trees[,1]/24/365,(Outflow_day_er_trees[,3]+Outflow_day_er_trees[,4])*ET_day_er_trees[,2]/24/365,cex=0.5,col="blue",type='l')
+par(new=T)
+plot(ET_day_er_trees[,1]/24/365,ET_day_er_trees[,6]/norm,cex=0.5,col='blue',type='p',axes=F,xlim=c(19,20.),ylim=c(0,1.5),xlab="",ylab="") 
+axis(1, at=c(19+1/12,19+3/12,19+5/12,19+7/12,19+9/12,19+11/12), labels=c("Oct","Dec","Feb","Apr","Jun","Aug"))
+
+plot(ET_day_lw_shrub[,1]/24/365,(ET_day_lw_shrub[,3]+ET_day_lw_shrub[,4]+ET_day_lw_shrub[,5])*ET_day_lw_shrub[,2]/24/365,cex=0.5,col="black",type='l',xlab='', ylab='ET [mm/d], ET Residence Time [y]',xlim=c(19,20.),ylim=c(0,1.5),main="C) LW Shrubs",xaxt='n')
+axis(1, at=c(19+1/12,19+3/12,19+5/12,19+7/12,19+9/12,19+11/12), labels=c("Oct","Dec","Feb","Apr","Jun","Aug"))
+
+par(new=T)
+plot(ET_day_lw_shrub[,1]/24/365,ET_day_lw_shrub[,6]/norm,cex=0.5,col='blue',type='p',axes=F,xlim=c(19,20.),ylim=c(0,1.5),xlab="",ylab="") 
+
+plot(ET_day_lw_trees[,1]/24/365,(ET_day_lw_trees[,3]+ET_day_lw_trees[,4]+ET_day_lw_trees[,5])*ET_day_lw_trees[,2]/24/365,cex=0.5,col="black",type='l',xlab="",xaxt='n', ylab='ET [mm/d], ET Residence Time [y]',xlim=c(19,20.),ylim=c(0,1.5),main="D) LW Trees")
+par(new=T)
+plot(ET_day_lw_trees[,1]/24/365,ET_day_lw_trees[,6]/norm,cex=0.5,col='blue',type='p',axes=F,xlim=c(19,20.),ylim=c(0,1.5),xlab="",ylab="") 
+
+axis(1, at=c(19+1/12,19+3/12,19+5/12,19+7/12,19+9/12,19+11/12), labels=c("Oct","Dec","Feb","Apr","Jun","Aug"))
+
+dev.off()
+
+
+
---
+++ Examples/Hillslope_Simulations/paper_cases/R_scripts/Waterfall_4panel_Plot.R
@@ -0,0 +1,167 @@
+### Waterfall and ET Hillslope plots
+### LEC, RMM 25-May-18, (edited 7-Sep-18)
+
+##This script loads in EcoSLIM output and makes Figures 4 and 5 from Maxwell et al Ecohydrology 2018
+
+library("MASS")
+library(fields)
+rm(list=ls())
+
+## you may need to edit the working directory 
+setwd('~/EcoSLIM/Examples/Hillslope_Simulations/paper_cases/R_scripts')
+source("./slicedens.R")
+
+months=c("OCT","NOV","DEC","JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP")
+
+## make waterfall four panel (Fig 4)
+fout = 'Figure4_waterfall_4-panel.pdf'
+pdf(file=fout,width=9, height=9)
+
+par(mfrow=c(2,2))
+
+#Read EcoSlim output files and put in independent matrices for plotting
+## load trees with ER forcing
+fin='./ER_hillslope_trees/SLIM_hillslope_ER_trees_exited_particles.bin'
+length=file.info(fin)$size/8
+ncol=8
+print(length)/ncol
+to.read = file(fin,'rb')
+part=matrix(0, nrow=(length/ncol), ncol=ncol)
+for(i in 1:(length/ncol)){
+	part[i,]= readBin(to.read, double(), endian='little', size=8,n=ncol)
+}
+close(to.read)
+colnames(part)=c("Time", "x", "y", "z", "Ptime", "Mass", "Comp", "ExitStatus")
+npart=nrow(part)
+
+#3D Histograms of particle ages
+out = which( (part[,8] == 1) & (part[,1]>=(19*8760)))
+et = which((part[,8] == 2) & (part[,1]>=(19*8760)))
+den3d_out_er_trees <- kde2d( (part[out,5])/24/365,part[out,1]/24/365, n = 50)
+
+den3d_et_er_trees_time <- kde2d( (part[et,5])/24/365,part[et,1]/24/365, n = 50)
+den3d_et_er_trees_space <- kde2d( (part[et,5])/24/365,part[et,2], n = 100)
+
+#Outflow
+x=part[out,5]/24/365
+y=part[out,1]/24/365
+z <- y
+trans=0.7
+fcol <- rbind(c(0,.1,.5,trans), c(.3,.8,.8,trans), c(1,1,0, trans))
+fcol=fcol[3:1,]
+lcol <- rbind(c(0,.3,.3,.8), c(.1,.1,.2,.7), c(0,0,1,.65))
+
+
+slicedens(x,y,z,fcol=fcol, bcol='white', lcol=lcol,gboost=.9, yinc=1, slices=12, xlab="Outflow Residence Time [YR] ",ylab="",main="A) ER Trees Outflow",yaxlab=months)
+        
+#ET
+x=part[et,5]/24/365
+y=part[et,1]/24/365
+z <- y
+
+slicedens(x,y,z,fcol=fcol, bcol='white', lcol=lcol,gboost=.90, yinc=7, slices=12, xlab="ET Residence Time [YR]",ylab="", main="B) ER Trees ET",yaxlab=months)
+
+
+## load shrubs with LW forcing
+fin='../LW_hillslope_shrub/SLIM_hillslope_LW_shrub_exited_particles.bin'
+length=file.info(fin)$size/8
+ncol=8
+print(length)/ncol
+to.read = file(fin,'rb')
+part=matrix(0, nrow=(length/ncol), ncol=ncol)
+for(i in 1:(length/ncol)){
+	part[i,]= readBin(to.read, double(), endian='little', size=8,n=ncol)
+}
+close(to.read)
+colnames(part)=c("Time", "x", "y", "z", "Ptime", "Mass", "Comp", "ExitStatus")
+npart=nrow(part)
+
+#3D Histograms of particle ages
+out = which( (part[,8] == 1) & (part[,1]>=(19*8760)))
+et = which((part[,8] == 2) & (part[,1]>=(19*8760)))
+den3d_out_lw_shrubs <- kde2d( (part[out,5])/24/365,part[out,1]/24/365, n = 50)
+den3d_et_lw_shrubs_time <- kde2d( (part[et,5])/24/365,part[et,1]/24/365, n = 50)
+den3d_et_lw_shrubs_space <- kde2d( (part[et,5])/24/365,part[et,2], n = 100)
+
+
+## load trees with LW forcing
+fin='../LW_hillslope_trees/SLIM_hillslope_LW_trees_exited_particles.bin'
+length=file.info(fin)$size/8
+ncol=8
+print(length)/ncol
+to.read = file(fin,'rb')
+part=matrix(0, nrow=(length/ncol), ncol=ncol)
+for(i in 1:(length/ncol)){
+	part[i,]= readBin(to.read, double(), endian='little', size=8,n=ncol)
+}
+close(to.read)
+colnames(part)=c("Time", "x", "y", "z", "Ptime", "Mass", "Comp", "ExitStatus")
+npart=nrow(part)
+
+#3D Histograms of particle ages
+out = which( (part[,8] == 1) & (part[,1]>=(19*8760)))
+et = which((part[,8] == 2) & (part[,1]>=(19*8760)))
+den3d_out_lw_trees <- kde2d( (part[out,5])/24/365,part[out,1]/24/365, n = 50)
+den3d_et_lw_trees_time <- kde2d( (part[et,5])/24/365,part[et,1]/24/365, n = 50)
+den3d_et_lw_trees_space <- kde2d( (part[et,5])/24/365,part[et,2], n = 100)
+
+#Outflow
+x=part[out,5]/24/365
+y=part[out,1]/24/365
+z <- y
+trans=0.7
+fcol <- rbind(c(0,.1,.5,trans), c(.3,.8,.8,trans), c(1,1,0, trans))
+fcol=fcol[3:1,]
+lcol <- rbind(c(0,.3,.3,.8), c(.1,.1,.2,.7), c(0,0,1,.65))
+
+
+slicedens(x,y,z,fcol=fcol, bcol='white', lcol=lcol,gboost=.9, yinc=1, slices=12, xlab="Outflow Residence Time [YR]",main="C) LW Trees Outflow",ylab="",yaxlab=months)
+          
+#ET
+x=part[et,5]/24/365
+y=part[et,1]/24/365
+z <- y
+
+slicedens(x,y,z,fcol=fcol, bcol='white', lcol=lcol,gboost=.90, yinc=7, slices=12, xlab="ET Residence Time [YR]",main="D) LW Trees ET",ylab="",yaxlab=months)
+
+dev.off()
+
+## load shrubs with ER forcing
+fin='./ER_hillslope_shrub/SLIM_hillslope_ER_shrub_exited_particles.bin'
+length=file.info(fin)$size/8
+ncol=8
+print(length)/ncol
+to.read = file(fin,'rb')
+part=matrix(0, nrow=(length/ncol), ncol=ncol)
+for(i in 1:(length/ncol)){
+	part[i,]= readBin(to.read, double(), endian='little', size=8,n=ncol)
+}
+close(to.read)
+colnames(part)=c("Time", "x", "y", "z", "Ptime", "Mass", "Comp", "ExitStatus")
+npart=nrow(part)
+
+#3D Histograms of particle ages
+out = which( (part[,8] == 1) & (part[,1]>=(19*8760)))
+et = which((part[,8] == 2) & (part[,1]>=(19*8760)))
+den3d_out_er_shrubs <- kde2d( (part[out,5])/24/365,part[out,1]/24/365, n = 50)
+den3d_et_er_shrubs_time <- kde2d( (part[et,5])/24/365,part[et,1]/24/365, n = 50)
+den3d_et_er_shrubs_space <- kde2d( (part[et,5])/24/365,part[et,2], n = 100)
+
+## make ET distribution four panel (Fig 5)
+fout = 'Figure5_ET_hillsllope_4-panel.pdf'
+pdf(file=fout)
+
+par(mfrow=c(2,2))
+
+image(den3d_et_er_shrubs_space,xlab = "ET residence time [yr]", ylab = "X Location Hillslope [m]",col = heat.colors(9),zlim=c(0.0001,0.05),xlim=c(0,10),breaks=c(0.000001,0.000005,0.00001,0.00005,0.0001,0.0005,0.001,0.005,0.01,0.05),main="A) ER Shrubs")
+
+image(den3d_et_er_trees_space,xlab = "ET residence time [yr]", ylab = "X Location Hillslope [m]",col = heat.colors(9),zlim=c(0.0001,0.05),xlim=c(0,10),breaks=c(0.000001,0.000005,0.00001,0.00005,0.0001,0.0005,0.001,0.005,0.01,0.05),main="B) ER Trees")
+
+image(den3d_et_lw_shrubs_space,xlab = "ET residence time [yr]", ylab = "X Location Hillslope [m]",col = heat.colors(9),zlim=c(0.0001,0.05),xlim=c(0,10),breaks=c(0.000001,0.000005,0.00001,0.00005,0.0001,0.0005,0.001,0.005,0.01,0.05),main="C) LW Shrubs")
+
+image(den3d_et_lw_trees_space,xlab = "ET residence time [yr]", ylab = "X Location Hillslope [m]",col = heat.colors(9),zlim=c(0.0001,0.05),xlim=c(0,10),breaks=c(0.000001,0.000005,0.00001,0.00005,0.0001,0.0005,0.001,0.005,0.01,0.05),main="D) LW Trees")
+
+image.plot(den3d_et_lw_trees_space, zlim=c(0.0001,0.05), cex=0.7,col=(heat.colors(9)), legend.only=T, legend.width=0.5, legend.shrink=0.5, legend.args=list(text='            Density [-]',cex=0.85,side=3,line=0.1), smallplot=c(.65,.68, .55,.75))
+dev.off()
+
+
